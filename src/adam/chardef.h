a37e63515a01f3dd5a5cd86a76334bb043337de3
Geoff Oltmans <oltmansg@gmail.com>
2026-01-03 21:24:57 -0600

    download fujinet-lib for adam

--- src/adam/chardef.h
+++ src/adam/chardef.h
@@ -2,20 +2,20 @@
 #define CHARDEF_H
 
 #define CH_HLINE        146
-#define CH_VLINE        124
-#define CH_ULCORNER     145
-#define CH_URCORNER     133
-#define CH_LLCORNER     154
-#define CH_LRCORNER     131
+#define CH_VLINE        252
+#define CH_ULCORNER     133
+#define CH_URCORNER     145
+#define CH_LLCORNER     131
+#define CH_LRCORNER     154
 #define CH_RTEE         132
-#define CH_LTEE         128
+#define CH_LTEE         129
 #define CH_TTEE         151
 #define CH_BTEE         152
-#define CH_CROSS        137
+#define CH_CROSS        147
 
-#define CH_CURS_LEFT    12
-#define CH_DEL          127
-#define CH_ENTER        10
+#define CH_CURS_LEFT    140
+#define CH_DEL          255
+#define CH_ENTER        138
 
 extern unsigned char bounce_udgs[];
 
